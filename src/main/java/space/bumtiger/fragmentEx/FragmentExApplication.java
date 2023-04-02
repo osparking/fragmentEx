7e967a572393c141fb52816ed92a3a444ed10bd7
osparking <jbpark03@gmail.com>
2023-04-02 18:29:29 +0900

    initial shape of 2nd fragment example

---
+++ src/main/java/space/bumtiger/fragmentEx/FragmentExApplication.java
@@ -0,0 +1,13 @@
+package space.bumtiger.fragmentEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragmentExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FragmentExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bumtiger/fragmentEx/FragmentExApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bumtiger.fragmentEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FragmentExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
